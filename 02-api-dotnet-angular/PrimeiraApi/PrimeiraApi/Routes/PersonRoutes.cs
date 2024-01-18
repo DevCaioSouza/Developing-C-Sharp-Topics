8ff09a9f427fa1a3a1006d6f92c76a7d05bfcc54
Caio Camargo <caiocbahia@hotmail.com>
2024-01-18 17:46:03 -0300

    decoupling routes to a new class

---
+++ 02-api-dotnet-angular/PrimeiraApi/PrimeiraApi/Routes/PersonRoutes.cs
@@ -0,0 +1,13 @@
+﻿namespace PrimeiraApi.Routes
+{
+	public static class PersonRoutes
+	{
+		public static void MapPersonRoutes(this WebApplication app)
+		{
+			app.MapGet("/hello-api", () =>
+			{
+				return new { greetings = "Hello, API!!" };
+			});
+		}
+	}
+}
