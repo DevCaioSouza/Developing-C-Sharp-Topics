5b8b43fbfaaa4a8d4ea34f0c72c5dd71711c74b3
Caio Camargo <caiocbahia@hotmail.com>
2024-01-19 14:52:59 -0300

    completed http methods

---
+++ 02-api-dotnet-angular/PrimeiraApi/PrimeiraApi/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace PrimeiraApi.Models
+{
+	public class Person
+	{
+		public Guid Id { get; set; }
+		public string Name { get; set; }
+
+		public Person(Guid id, string name)
+		{
+			Id = id;
+			Name = name;
+		}
+	}
+}
--- 02-api-dotnet-angular/PrimeiraApi/PrimeiraApi/Routes/PersonRoutes.cs
+++ 02-api-dotnet-angular/PrimeiraApi/PrimeiraApi/Routes/PersonRoutes.cs
@@ -1,13 +1,43 @@
-﻿namespace PrimeiraApi.Routes
+﻿using PrimeiraApi.Models;
+
+namespace PrimeiraApi.Routes
 {
 	public static class PersonRoutes
 	{
+		public static List<Person> PersonList = new()
+		{
+			new Person(id: Guid.NewGuid(), name:"Neymar"),
+			new Person(id: Guid.NewGuid(), name:"Suarez"),
+			new Person(id: Guid.NewGuid(), name:"Messi"),
+		};
 		public static void MapPersonRoutes(this WebApplication app)
 		{
-			app.MapGet("/hello-api", () =>
+			app.MapGet("/personlist", () => PersonList );
+
+			app.MapGet("/personlist/{name}", 
+				(string name) => PersonList.Find(x => x.Name == name));
+
+			app.MapPost("/personlist", (HttpContext request ,Person person) =>
 			{
-				return new { greetings = "Hello, API!!" };
+
+				var nome = request.Request.Query["name"];
+
+				PersonList.Add(person);
+				return Results.Ok(person);
 			});
+
+			app.MapPut("/personlist/{id}", 
+				(Guid id, Person person) =>
+				{
+					var findPerson = PersonList.Find(x => x.Id == id);
+
+					if (findPerson == null)
+						return Results.NotFound();
+
+					findPerson.Name = person.Name;
+
+					return Results.Ok(person);
+				} );
 		}
 	}
 }
