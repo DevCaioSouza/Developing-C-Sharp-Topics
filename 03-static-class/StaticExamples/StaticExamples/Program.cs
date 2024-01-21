9e8f338a4a556cd3e91aafbb889d38e32dd55ac5
Caio Camargo <caiocbahia@hotmail.com>
2024-01-20 21:02:39 -0300

    Before setting Car class as static

---
+++ 03-static-class/StaticExamples/StaticExamples/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.DataAnnotations;
+
+class Program
+{
+	public static void Main(string[] args)
+	{
+		Car car1 = new Car("Opala");
+		Car car2 = new Car("Monza");
+
+		Console.WriteLine(Car.numberOfCars);
+
+		Car.StartRace();
+
+		Console.ReadKey();
+	}
+}
+
+class Car
+{
+	String Model;
+
+	public static int numberOfCars;
+
+	public Car (string model)
+	{
+		this.Model = model;
+		numberOfCars++;
+	}
+
+	public static void StartRace()
+	{
+		Console.WriteLine("The race has started!!");
+	}
+}
