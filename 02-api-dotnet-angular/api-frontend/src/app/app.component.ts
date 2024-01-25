a68596146be4775a539ea4e0386517b2d6f938ea
Caio Camargo <caiocbahia@hotmail.com>
2024-01-25 15:53:50 -0300

    getting players from the api using the front end

--- 02-api-dotnet-angular/api-frontend/src/app/app.component.ts
+++ 02-api-dotnet-angular/api-frontend/src/app/app.component.ts
@@ -3,31 +3,39 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { Player } from './models/player';
+import { Observable } from 'rxjs';
+import { MatTableModule } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MatTableModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  host: {ngSkipHydration: 'true'},
 })
-export class AppComponent{
-
+export class AppComponent {
   // ngOnInit(): void {
   //   this.getPlayerList();
   // }
 
+  constructor(private http: HttpClient) {}
+
   title = 'api-frontend';
 
-  http = inject(HttpClient);
+  // http = inject(HttpClient);
 
   url = 'http://localhost:5170/';
 
-  playerList: Player[] = [];
+  playerList$?: Observable<Player[]>;
 
   getPlayerList() {
-    this.http
-      .get<Player[]>(`${this.url}playerlist`)
-      .subscribe((playerList) => (this.playerList = playerList));
+    this.playerList$ = this.http.get<Player[]>(`${this.url}playerlist`);
+  }
+
+  shouldSayHello: boolean = true;
+
+  switchHello() {
+    this.shouldSayHello = !this.shouldSayHello;
   }
 }
