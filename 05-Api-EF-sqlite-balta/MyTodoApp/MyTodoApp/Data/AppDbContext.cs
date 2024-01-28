ae6c9a51b54ef13f7b72269b8bfea4f1c4689eff
Caio Camargo <caiocbahia@hotmail.com>
2024-01-28 16:46:53 -0300

    DbSet & Connection String

---
+++ 05-Api-EF-sqlite-balta/MyTodoApp/MyTodoApp/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyTodoApp.Models;
+
+namespace MyTodoApp.Data
+{
+	public class AppDbContext : DbContext
+	{
+        public DbSet<Todo> Todos { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
+		}
+	}
+}
